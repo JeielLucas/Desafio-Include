196e1d82c561a19e90e04a1f94218971441aef8a
Jeiel <jeiellucas0404@gmail.com>
2023-09-12 18:59:46 -0300

    Atualizando todo o repo

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const button = document.getElementById('menuButton');
+const buttonFechar = document.getElementById('botaoFechar');
+const nav = document.getElementById('nav');
+const botaoMobile = document.querySelectorAll(".botaoMobile");
 
 function toggleMenu(){
-    const nav = document.getElementById('nav');
     nav.classList.toggle('active');
-    
 }
 
+function fecharMenuMobile(){
+    if(nav.classList.contains('active')){
+        nav.classList.remove('active');
+    }
+}
+
+botaoMobile[0].addEventListener('click', fecharMenuMobile);
+botaoMobile[1].addEventListener('click', fecharMenuMobile);
+botaoMobile[2].addEventListener('click', fecharMenuMobile);
 button.addEventListener('click', toggleMenu);
+buttonFechar.addEventListener('click', toggleMenu);
